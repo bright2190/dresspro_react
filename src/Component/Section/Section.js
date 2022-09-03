81c569f93aa1c5d66203102edaeaf3121a137cc4
baniex <banjibaniex1@gmail.com>
2022-09-03 19:43:00 +0100

    working on specifics

--- src/Component/Section/Section.js
+++ src/Component/Section/Section.js
@@ -1,11 +1,96 @@
 import React from 'react'
 import "./Section.css"
+import "./SectionBackgroundImage.scss"
 import { Policy } from '../Policy/Policy'
 
 const Section = (props) => {
+
+  const mouse_hover = (event)=>{
+    event.currentTarget.querySelector(".mini_skirts_image_hover").style.display = "block"
+    // alert("hi")
+
+    console.log("hello")
+  }
+
+  const mouse_leave = (event) => {
+    event.currentTarget.querySelector(".mini_skirts_image_hover").style.display = "none"
+    // alert("hi")
+
+    console.log("hello");
+  };
+
+
+
+
+
+
+
+
+
+
+
+  let cloth_name = props.cloth_name;
+  let number_of_clothes = props.number_of_clothes;
+  let cloth_name_lowercase = cloth_name.toLowerCase();
+  let number_of_clothes_div = [
+    <div class="mini_skirts_image_container" onMouseEnter={mouse_hover} onMouseLeave={mouse_leave}>
+      <div class="mini_skirts_image">
+        <div class="mini_skirts_image_hover">
+          <div class="image_identity">MS10001</div>
+          <div class="buy_now">BUY NOW</div>
+        </div>
+      </div>
+    </div>
+  ];
+  for (let i = 0; i < number_of_clothes; i++){
+    number_of_clothes_div.push(
+      <div class="mini_skirts_image_container">
+        <div class="mini_skirts_image">
+          <div class="mini_skirts_image_hover">
+            <div class="image_identity">MS10001</div>
+            <div class="buy_now">BUY NOW</div>
+          </div>
+        </div>
+      </div>
+    );
+
+  }
   return (
-    <div><div>Section</div>
-    <div>{props.cloth_name}</div>
+    <div>
+      
+      <div class="mini_skirts_info">
+        <div class="mini_skirts_nav_display">Products {cloth_name} </div>
+        <div class="mini_skirts_text_display">MINI SKIRTS</div>
+      </div>
+
+      <div class="mini_skirts_image_section">
+        <div class="mini_skirts_image_container">
+          <div class="mini_skirts_image">
+            <div class="mini_skirts_image_hover">
+              <div class="image_identity">MS10001</div>
+              <div class="buy_now">BUY NOW</div>
+            </div>
+          </div>
+        </div>
+        <div class="mini_skirts_image_container">
+          <div class="mini_skirts_image">
+            <div class="mini_skirts_image_hover">
+              <div class="image_identity">MS10001</div>
+              <div class="buy_now">BUY NOW</div>
+            </div>
+          </div>
+        </div>
+        <div class="mini_skirts_image_container">
+          <div class="mini_skirts_image">
+            <div class="mini_skirts_image_hover">
+              <div class="image_identity">MS10001</div>
+              <div class="buy_now">BUY NOW</div>
+            </div>
+          </div>
+        </div>
+        {number_of_clothes_div}
+      </div>
+
     </div>
 
   )
--- src/Component/SpaceAtTheSide/SpaceAtTheSide.js
+++ src/Component/SpaceAtTheSide/SpaceAtTheSide.js
@@ -30,17 +30,13 @@ export default function SpaceAtTheSide() {
     
   ];
 
+  let cloth_categories_number = [25, 26, 36, 30, 32, 30]
+
   let cloth_route =  cloth_categories.map((cloth_name, index) =>{
-
-
     let cloth_name_id = cloth_name.replace(/\s+/g, "").toLowerCase();
-
-
-    
-
-
+    let number_of_clothes = cloth_categories_number[index]
     return (
-          <Route path={"/" + cloth_name_id} element={<Section cloth_name={cloth_name}/>}></Route>
+          <Route path={"/" + cloth_name_id} element={<Section cloth_name={cloth_name} number_of_clothes={number_of_clothes}/>}></Route>
 
     )
   })
@@ -59,12 +55,10 @@ export default function SpaceAtTheSide() {
         <Navbar navbar_togglerProps={navbar_toggler} />
         <Routes>
           {cloth_route}
-
           <Route path="/" element={<Home cloth_categories={cloth_categories}  />}></Route>
           <Route path="/customer" element={<CustomerReview />}></Route>
           <Route path="/about" element={<AboutUs />}></Route>
           <Route path="/policy" element={<Policy />}></Route>
-          {cloth_route}
         </Routes>
         <Footer />
       </div>
